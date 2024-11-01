3c9b4655035a58616c0efb3f38f929949d63f401
gibigbig@hotmail.com <gibigbig@hotmail.com>
2024-11-01 14:41:52 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
+
+  const [chapters, setChapters] = useState([]);
+  const [selectedChapter, setSelectedChapter] = useState(1);
+  const [verses, setVerses] = useState([]);
+
+  const getChapters = async () => {
+    const response = await fetch('https://api.quran.com/api/v4/chapters');
+    const data = await response.json();
+    setChapters(data.chapters);
+  }
+
+  const getVerses = async () => {
+    const response = await fetch(`https://api.quran.com/api/v4/verses/by_chapter/${selectedChapter}?words=true`);
+    const data = await response.json();
+    setVerses(data.verses);
+  }
+
+  useEffect(() => {
+    getChapters();
+    getVerses();
+  }, []);
+
+  console.log(verses);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App flex flex-col">
+      <header className="App-header flex-0 bg-gray-100 p-3">
+        <div>
+          <select>
+            {chapters.map(chapter => (
+              <option key={chapter.id} value={chapter.id}>{chapter.name_simple}</option>
+            ))}
+          </select>
+        </div>
       </header>
+      <main className="flex-1 p-4 flex flex-wrap flex-row">
+        {verses.map(verse => (
+          verse.words.map(word => {
+            return <Textbit key={verse.id} arabic={word.text} transliteration={word.transliteration.text} translation={word.translation.text} />
+          })
+        ))}
+      </main>
     </div>
   );
 }
+
+function Textbit({ arabic, transliteration, translation }) {
+  return (
+    <div className="px-4 py-2 text-center">
+      <div className="p-2 text-2xl font-bold noto-sans-arabic">{arabic}</div>
+      <div className="p-1 text-md">{transliteration}</div>
+      <div className="p-1 text-md">{translation}</div>
+    </div>
+  )
+}
+
 
 export default App;
