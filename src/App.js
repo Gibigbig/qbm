9fe3808bae944a7b3ce204d04265a131752c4512
gibigbig@hotmail.com <gibigbig@hotmail.com>
2024-11-05 15:45:16 -0400

    changes to the reader

--- src/App.js
+++ src/App.js
@@ -14,9 +14,14 @@ function App() {
   }
 
   const getVerses = async () => {
-    const response = await fetch(`https://api.quran.com/api/v4/verses/by_chapter/${selectedChapter}?words=true`);
+    const response = await fetch(`https://api.quran.com/api/v4/verses/by_chapter/${selectedChapter}?words=true&fields=text_uthmani&word_fields=text_uthmani&per_page=300&page=1`);
     const data = await response.json();
     setVerses(data.verses);
+  }
+
+  const changeChapter = (e) => {
+    setVerses([]);
+    setSelectedChapter(e.target.value);
   }
 
   useEffect(() => {
@@ -24,20 +29,23 @@ function App() {
     getVerses();
   }, []);
 
-  console.log(verses);
+  useEffect(() => {
+    getVerses();
+  }, [selectedChapter]);
+
 
   return (
-    <div className="App flex flex-col">
+    <div className="App flex flex-col" dir="rtl">
       <header className="App-header flex-0 bg-gray-100 p-3">
         <div>
-          <select>
+          <select onChange={changeChapter}>
             {chapters.map(chapter => (
               <option key={chapter.id} value={chapter.id}>{chapter.name_simple}</option>
             ))}
           </select>
         </div>
       </header>
-      <main className="flex-1 p-4 flex flex-wrap flex-row">
+      <main className="flex-1 p-4 table">
         {verses.map(verse => (
           verse.words.map(word => {
             return <Textbit key={verse.id} arabic={word.text} transliteration={word.transliteration.text} translation={word.translation.text} />
@@ -50,10 +58,10 @@ function App() {
 
 function Textbit({ arabic, transliteration, translation }) {
   return (
-    <div className="px-4 py-2 text-center">
-      <div className="p-2 text-2xl font-bold noto-sans-arabic">{arabic}</div>
-      <div className="p-1 text-md">{transliteration}</div>
-      <div className="p-1 text-md">{translation}</div>
+    <div className="px-4 py-2 text-center inline-block">
+      <div className="p-2 text-5xl  amiri-quran-regular">{arabic}</div>
+      <div className="p-1 text-sm mt-8">{transliteration}</div>
+      <div className="p-1 text-sm">{translation}</div>
     </div>
   )
 }
